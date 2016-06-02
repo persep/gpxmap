a85f4843f8a293c43c05f5a5783750417794970f
PerseP <persep@gmail.com>
2016-06-02 11:03:28 +0200

    add gpx parsing to model

--- app/models/track.rb
+++ app/models/track.rb
@@ -17,10 +17,56 @@ class Track < ActiveRecord::Base
 	# or any other validator See: https://github.com/thoughtbot/paperclip/pull/1554
 	after_validation :clean_paperclip_errors
 
-	before_save :parse_file
+	before_save :parse_file #This callback executes the parser function every time we upload a file.
 
 	private
   		def clean_paperclip_errors
     		errors.delete(:gpx_file_name)
 	  	end
+
+	  	def parse_file
+	  		tempfile = gpx.queued_for_write[:original]
+	  		byebug
+	  		doc = Nokogiri::XML(tempfile)
+	  		parse_xml(doc)
+	  	end
+
+	  	def parse_xml(doc)
+	  		doc.root.elements.each do |node|
+	  			parse_tracks(node)
+	  		end
+	  	end
+
+	  	def parse_tracks(node)
+	  		if node.node_name.eql? 'trk'
+	  			node.elements.each do |node|
+	  				parse_track_segments(node)
+	  			end
+    		end
+    	end
+
+    	def parse_track_segments(node)
+    		if node.node_name.eql? 'trkseg'
+    			tmp_segment = Tracksegment.new
+    			node.elements.each do |node|
+    				parse_points(node,tmp_segment)
+    			end
+    			self.tracksegments << tmp_segment
+    		end
+  		end
+
+  		def parse_points(node,tmp_segment)
+  			if node.node_name.eql? 'trkpt'
+  				tmp_point = Point.new
+  				tmp_point.latitude = node.attr("lat")
+  				tmp_point.longitude = node.attr("lon")
+  				node.elements.each do |node|
+  					tmp_point.name = node.text.to_s if node.name.eql? 'name'
+  					tmp_point.elevation = node.text.to_s if node.name.eql? 'ele'
+  					tmp_point.description = node.text.to_s if node.name.eql? 'desc'
+  					tmp_point.point_created_at = node.text.to_s if node.name.eql? 'time'
+  				end
+  				tmp_segment.points << tmp_point
+  			end
+  		end
 end
