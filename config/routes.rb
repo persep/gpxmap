35097d7daf406c885347ec2cb695dde48d357680
PerseP <persep@gmail.com>
2016-06-02 11:58:08 +0200

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'tracks#index'
   resources :tracks
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
