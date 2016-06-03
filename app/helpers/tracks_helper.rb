2c9eb55b87b9ace02bb7341acd5a299f61168645
PerseP <persep@gmail.com>
2016-06-03 14:54:56 +0200

    Add map display

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,18 @@
 module TracksHelper
+	def google_maps_api_key
+		"AIzaSyCBUCpPgwZWFtpTgaiv-QMvug54Dn4vmY8"
+	end
+
+	def google_api_url
+		"http://maps.googleapis.com/maps/api/js"
+	end
+
+	def google_api_access
+		"#{google_api_url}?key=#{google_maps_api_key}&libraries=geometry&sensor=false"
+	end
+
+	def google_maps_api
+		content_tag(:script,:type => "text/javascript",:src => google_api_access) do
+		end
+	end
 end
